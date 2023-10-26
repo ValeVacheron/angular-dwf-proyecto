ace359226eadcc82000b80dcef1e3257bf3b2b11
eduardo-reyes <eduardoreyes@ciencias.unam.mx>
2023-10-25 23:29:16 -0600

    Servicio category creado

---
+++ src/app/modules/product/_services/category.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Category } from '../_models/category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor() { }
+}
